c4046463d8e533df45e10ab7faa6230fcadd91c8
Alexey Kamenskiy <akamensky@users.noreply.github.com>
2023-01-21 00:56:34 +1100

    Inline documentation

--- error.go
+++ error.go
@@ -2,10 +2,12 @@ package envparse
 
 import "fmt"
 
+// ErrorList implements `error` interface with ability to combine multiple errors togethers
 type ErrorList struct {
 	errors []error
 }
 
+// FullError will return a new error made using `(*ErrorList).FullError()` or will return original `error` otherwise
 func FullError(err error) error {
 	if errorList, ok := err.(*ErrorList); ok {
 		return fmt.Errorf(errorList.FullError())
@@ -18,10 +20,12 @@ func newErrorList() *ErrorList {
 	return &ErrorList{errors: make([]error, 0)}
 }
 
+// Error is needed to implement `error` interface
 func (e *ErrorList) Error() string {
 	return fmt.Sprintf("%d errors happened during parsing environment variables", len(e.errors))
 }
 
+// FullError combined all errors together in single message
 func (e *ErrorList) FullError() string {
 	msg := e.Error()
 	if len(e.errors) > 0 {
@@ -33,6 +37,7 @@ func (e *ErrorList) FullError() string {
 	return msg
 }
 
+// IsEmpty reports if *ErrorList is empty (0 appended errors)
 func (e *ErrorList) IsEmpty() bool {
 	if e.errors == nil || len(e.errors) == 0 {
 		return true
@@ -41,6 +46,7 @@ func (e *ErrorList) IsEmpty() bool {
 	return false
 }
 
+// Append adds a new error to the stack
 func (e *ErrorList) Append(err error) {
 	e.errors = append(e.errors, err)
 }
--- options.go
+++ options.go
@@ -5,10 +5,14 @@ import (
 	"strings"
 )
 
+// SetPrefix sets new prefix to use for scanning. Default is `APP`
 func SetPrefix(s string) {
 	defaultPrefix = strings.ToUpper(regexp.MustCompile(`[^A-Za-z0-9]`).ReplaceAllString(s, ""))
 }
 
+// SetMaxDepth sets maximum embedded depth allowed for structs and slices.
+// Crossing this limit will cause an error. Serves as a fail-safe against accidental infinite loops.
+// Default is 100
 func SetMaxDepth(i int) {
 	defaultMaxDepth = i
 }
