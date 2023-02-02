fa3b5d643608d80a48d528200f7561786ae9e9aa
Alexey Kamenskiy <akamensky@users.noreply.github.com>
2023-02-03 00:13:16 +1100

    * Allow underscores in tag

--- tag_test.go
+++ tag_test.go
@@ -76,3 +76,32 @@ func TestParseTag(t *testing.T) {
 		t.Errorf("expected error to be '%s', but got '%s'", expectedErr, err)
 	}
 }
+
+func TestParseTag2(t *testing.T) {
+	// Test allowing underscores in `name`
+	var in string
+	var expectedOut *tagType
+	var expectedErr error
+
+	in = "name=HELLO_1234"
+	expectedOut = &tagType{name: "HELLO_1234", required: false, defaultValue: ""}
+	expectedErr = nil
+	tag, err := parseTag(in, "")
+	if !reflect.DeepEqual(tag, expectedOut) {
+		t.Errorf("expected tag to be %v, but got %v", expectedOut, tag)
+	}
+	if expectedErr != err {
+		t.Errorf("expected error to be '%s', but got '%s'", expectedErr, err)
+	}
+
+	in = "name=_HELLO_1234_"
+	expectedOut = &tagType{name: "HELLO_1234", required: false, defaultValue: ""}
+	expectedErr = nil
+	tag, err = parseTag(in, "")
+	if !reflect.DeepEqual(tag, expectedOut) {
+		t.Errorf("expected tag to be %v, but got %v", expectedOut, tag)
+	}
+	if expectedErr != err {
+		t.Errorf("expected error to be '%s', but got '%s'", expectedErr, err)
+	}
+}
